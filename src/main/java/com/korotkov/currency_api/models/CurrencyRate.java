1da9d67b2dd029ce3056864f9c4356adf9dc685a
VladislavKortokov <vladislav.korotkow@gmail.com>
2024-06-30 17:21:04 +0300

    feat(currency): add CurrencyRate entity and repository

---
+++ src/main/java/com/korotkov/currency_api/models/CurrencyRate.java
@@ -0,0 +1,25 @@
+package com.korotkov.currency_api.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CurrencyRate {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private int curId;
+    private String date;
+    private String curAbbreviation;
+    private int curScale;
+    private String curName;
+    private double curOfficialRate;
+}
