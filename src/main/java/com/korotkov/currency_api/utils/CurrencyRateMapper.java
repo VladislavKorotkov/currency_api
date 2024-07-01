5adca42ec62d108e4f0f5b45e2d493686a3147c0
VladislavKortokov <vladislav.korotkow@gmail.com>
2024-07-01 12:12:58 +0300

    feat(currency-service): add CurrencyRateLoaderService and CurrencyRateService

--- src/main/java/com/korotkov/currency_api/utils/CurrencyRateMapper.java
+++ src/main/java/com/korotkov/currency_api/utils/CurrencyRateMapper.java
@@ -5,9 +5,12 @@ import com.korotkov.currency_api.responses.CurrencyRateResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
+import java.util.List;
+
 @Mapper
 public interface CurrencyRateMapper {
     CurrencyRateMapper INSTANCE = Mappers.getMapper(CurrencyRateMapper.class);
 
     CurrencyRateResponse currencyRateToCurrencyRateResponse(CurrencyRate currencyRate);
+    List<CurrencyRateResponse> toCurrencyResponseList(List<CurrencyRate> byDate);
 }
