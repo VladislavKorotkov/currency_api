7515387cb32c67e6e9d7eeb5832d624e03a91480
VladislavKortokov <vladislav.korotkow@gmail.com>
2024-07-01 12:11:51 +0300

    refactor(currency): update CurrencyRate model and repository

--- src/main/java/com/korotkov/currency_api/repositories/CurrencyRateRepository.java
+++ src/main/java/com/korotkov/currency_api/repositories/CurrencyRateRepository.java
@@ -4,8 +4,9 @@ import com.korotkov.currency_api.models.CurrencyRate;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CurrencyRateRepository extends JpaRepository<CurrencyRate, Long> {
     List<CurrencyRate> findByDate(String date);
-    CurrencyRate findByDateAndCurAbbreviation(String date, String curAbbreviation);
+    Optional<CurrencyRate> findByDateAndCurId(String date, int currId);
 }
